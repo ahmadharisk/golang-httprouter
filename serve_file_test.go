013340116b5256abfe4fd026acba1e62c784225d
Peppo <ahmadharisk@gmail.com>
2025-10-18 10:57:34 +0700

    Hari 30, belajar library http router penggunaan serve file

---
+++ serve_file_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"embed"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"io/fs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+//go:embed resources
+var resources embed.FS
+
+func TestServeFile(t *testing.T) {
+
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	request := httptest.NewRequest("GET", "http://localhost:8080/files/hello.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "hello dari txt", string(body))
+}
+
+func TestServeFileGoodbye(t *testing.T) {
+
+	router := httprouter.New()
+	directory, _ := fs.Sub(resources, "resources")
+	router.ServeFiles("/files/*filepath", http.FS(directory))
+
+	request := httptest.NewRequest("GET", "http://localhost:8080/files/goodbye.txt", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "goodbye dari txt", string(body))
+}
