441d1c64993d6209533db6d40f0959f67243da9e
Peppo <ahmadharisk@gmail.com>
2025-10-18 10:35:29 +0700

    Initial commit

---
+++ params_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestRouter(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/", func(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
+		fmt.Fprint(writer, "Hello world")
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:8080/", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	assert.Equal(t, "Hello world", string(body))
+}
